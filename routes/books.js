9b9681411d404e11956caabb1d309ad7a022040c
faquinoortiz <144743802+faquinoortiz@users.noreply.github.com>
2023-11-20 10:21:53 -0800

    updated routes

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { Book } = require('../models');
 
 // Show the full list of books
-router.get('/books', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const books = await Book.findAll();
     res.render('books/index', { books });
@@ -13,12 +13,12 @@ router.get('/books', async (req, res, next) => {
 });
 
 // Show the create new book form
-router.get('/books/new', (req, res) => {
+router.get('/new', (req, res) => {
   res.render('books/new-book');
 });
 
 // Post a new book to the database
-router.post('/books/new', async (req, res, next) => {
+router.post('/new', async (req, res, next) => {
   try {
     await Book.create(req.body);
     res.redirect('/books');
@@ -33,7 +33,7 @@ router.post('/books/new', async (req, res, next) => {
 });
 
 // Show book detail form
-router.get('/books/:id', async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const book = await Book.findByPk(req.params.id);
     if (book) {
@@ -47,7 +47,7 @@ router.get('/books/:id', async (req, res, next) => {
 });
 
 // Update book info in the database
-router.post('/books/:id', async (req, res, next) => {
+router.post('/:id', async (req, res, next) => {
   try {
     const book = await Book.findByPk(req.params.id);
     if (book) {
@@ -67,7 +67,7 @@ router.post('/books/:id', async (req, res, next) => {
 });
 
 // Delete a book
-router.post('/books/:id/delete', async (req, res, next) => {
+router.post(':id/delete', async (req, res, next) => {
   try {
     const book = await Book.findByPk(req.params.id);
     if (book) {
